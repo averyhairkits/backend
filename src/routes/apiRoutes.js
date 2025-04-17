ac074a1601b3ac9f586086c8e60adec59c075ca5
“Kaiwen <kevin9966hu@gmail.com>
2025-04-17 00:06:07 -0500

    added the grant admin role controller function

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { newRequestController, approveRequestController, rejectRequestController } = require('../controllers/apiController');
+const { grantAdminController } = require('../controllers/grantAdminController');
 
 // client request to volunteer on specific date and time
 router.post("/new_request", newRequestController);
 router.post("/approve_request", approveRequestController)
 router.post("/reject_request", rejectRequestController)
+router.post("/grant_admin", grantAdminController)
 
 module.exports = router;
