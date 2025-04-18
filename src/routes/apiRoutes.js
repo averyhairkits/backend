8d29e8517a5f6c6364e564a8cac66509178ba1f0
Lizyum <lizbethyumbla2025@u.northwestern.edu>
2025-04-17 23:02:42 -0500

    Resolved lint errors, except 1
    When running npm run lint, you can see the error on line 119 was not assigned a value

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< Updated upstream
 const { newRequestController, approveRequestController, rejectRequestController } = require('../controllers/apiController');
 const { grantAdminController } = require('../controllers/grantAdminController');
 
@@ -8,5 +9,17 @@ router.post("/new_request", newRequestController);
 router.post("/approve_request", approveRequestController)
 router.post("/reject_request", rejectRequestController)
 router.post("/grant_admin", grantAdminController)
+=======
+const {
+  newRequestController,
+  approveRequestController,
+  rejectRequestController,
+} = require('../controllers/apiController');
+
+// client request to volunteer on specific date and time
+router.post('/new_request', newRequestController);
+router.post('/approve_request', approveRequestController);
+router.post('/reject_request', rejectRequestController);
+>>>>>>> Stashed changes
 
 module.exports = router;
